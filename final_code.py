e9530e206b5f1f10658ce9ed3e1edd3ebd46db9a
Mohamed Helal <mohamedhanyhelall@gmail.com>
2026-09-16 17:43:22 +0300

    dataset added

--- final_code.py
+++ final_code.py
@@ -3,3 +3,5 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 import streamlit as st
+df=pd.read_csv('healthcare-dataset-stroke-data-selected-columns.csv')
+print(df.head())
